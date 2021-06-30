3cdcf02dbf4d8a7a87cdf3851c5ee98d3143dc59
9bason <86515103+9bason@users.noreply.github.com>
2021-06-30 11:47:03 +0500

    new

--- TSIS2_1.py
+++
@@ -1,3 +0,0 @@
-valid_ip = input("address = ")
-defanged_ip = valid_ip.replace(".", "[.]")
-	print(defanged_ip)
--- TSIS2_2.py
+++
@@ -1,5 +0,0 @@
-command = input("command = ")
-command = command.replace("()", "o")
-command = command.replace("(al)", "al")
-
-print(command)
